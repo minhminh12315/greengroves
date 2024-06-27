7fffcdb4077c7ff3ce80adcceb5fe952c08e49c0
phanthanhcongo <cong.pt.2412@aptechlearning.edu.vn>
2024-06-27 16:32:13 +0700

    Header xong

--- resources/views/livewire/user/header.blade.php
+++ resources/views/livewire/user/header.blade.php
@@ -2,5 +2,73 @@
     <a wire:navigate href="/">Home</a>
     <a wire:navigate href="/about">About</a>
     <a wire:navigate href="/contact">Contact</a>
-    <a wire:navigate href="/login">login</a>
+    <header class="header">
+        <div class="containerHeader">
+            <div class="row align-items-center">
+                <div class="col-1 p-2 Logo">
+                    <div class="wrapper-container">
+                        <div class="wrapper-item">
+                            <div class="logo-container">
+                                <img class="logo" src="./logo.webp"
+                                    alt="logo">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2"></div>
+                <div class="col-1 Menu">
+                    <div class="wrapper-container">
+                        <div class="wrapper-item">
+                            <div class="menuContainer">
+                                <button class="btn btnMenu">Menu</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1 SearchBar_Middle ">
+                    <div class="wrapper-container">
+                        <div class="wrapper-item">
+                            <div class="searchContainerMiddle">
+                                <div class="iconSearchMiddle">
+                                    <i class="fa-solid fa-search middleSearchIcon"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 d-none d-md-block SearchBar">
+                    <div class="wrapper-container">
+                        <div class="wrapper-item">
+                            <div class="searchContainer">
+                                <div class="iconSearch">
+                                    <i class="fa-solid fa-search"></i>
+                                </div>
+                                <input type="text" class="formSearch">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1 Login">
+                    <div class="wrapper-container">
+                        <div class="wrapper-item">
+                            <button
+                                class="btn btn-danger btnLogin">Login</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1 CartShop">
+                    <div class="wrapper-container">
+                        <div class="wrapper-item">
+                            <div class="cartShop-container">
+                                <div class="iconCartShop">
+                                    <i  class="fa-solid fa-cart-shopping"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </div>
+
