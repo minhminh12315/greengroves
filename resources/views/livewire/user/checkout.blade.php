dc261f0a995a7792fe2f66a4e2b1989c1f423d36
nerogi <f17051@hvnh.edu.vn>
2024-07-04 10:28:20 +0700

    Quan:checkout css final

--- resources/views/livewire/user/checkout.blade.php
+++ resources/views/livewire/user/checkout.blade.php
@@ -1,81 +1,96 @@
 @extends('livewire.user.index')
 @section('content')
-<div class="checkoutPage container">
-    <div class="delivery p-3">
-        <h2 class="mb-5">Delivery</h2>
-        <form class="form-checkout">
-            <div class="row mb-5">
-                <div class="col-7 checkout-box input-group-lg">
-                    <input type="text" class="form-control" id="firstName" required>
-                    <label for="firstName">First Name</label>
+<div class="container">
+    <form class="row row-cols-md-2 row-cols-sm-1">
+        <div class="col-md-8 col-sm-12">
+            <h3 class="mb-4">Checkout</h3>
+            <div class="form-checkout">
+                <div class="row mb-4">
+                    <div class="col-sm-12 col-md-6">
+                        <label class="p-1" for="email">Email*</label>
+                        <input type="text" class="form-control p-1" name="email" required>
+                    </div>
                 </div>
-                <div class="col-5 checkout-box input-group-lg">
-                    <input type="text" class="form-control" id="lastName" required>
-                    <label for="lastName">Last Name</label>
+                <h4 class="mb-4">Delivery</h4>
+                <div class="row mb-4">
+                    <div class="col-md-6 col-sm-12">
+                        <label class="p-1" for="name">Fullname*</label>
+                        <input type="text" class="form-control p-1" name="name" required>
+                    </div>
+                </div>
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="col-md-6 col-sm-12 mb-4">
+                        <label class="p-1" for="phone">Phone Number*</label>
+                        <input type="text" class="form-control p-1" name="phone" required>
+            </div>
+                    <div class="col-md-6 col-sm-12 mb-4">
+                        <label class="p-1" for="address">Address*</label>
+                        <input type="text" class="form-control p-1" name="address" required>
+                    </div>
+                </div>
+                <div class="row row-cols-md-2 row-cols-sm-1">
+                    <div class="col-md-6 col-sm-12 mb-4">
+                        <label class="p-1" for="street">Distric*</label>
+                        <input type="text" class="form-control p-1" name="street" required>
+                    </div>
+                    <div class="col-md-6 col-sm-12 mb-4">
+                        <label class="p-1" for="city">City*</label>
+                        <input type="text" class="form-control p-1" name="city" required>
+                    </div>
+                </div>
+                <div class="col-12 mb-4">
+                    <label class="p-1" for="Name">Notes - Optional</label>
+                    <textarea class="form-control p-1" name="name"></textarea>
                 </div>
             </div>
-            <div class="row mb-5">
-                <div class="col-8 checkout-box input-group-lg">
-                    <input type="text" class="form-control" id="address" required>
-                    <label for="address">Address</label>
-                </div>
-                <div class="col-4 checkout-box input-group-lg">
-                    <input type="text" class="form-control" id="city" required>
-                    <label for="city">City</label>
-                </div>
-            </div>
-            <div class="row mb-3 ">
-                <div class="col-6 checkout-box input-group-lg">
-                    <input type="text" class="form-control" id="phone" required>
-                    <label for="phone">Phone</label>
-                </div>
-            </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input border border-secondary"></input>
-                <span class="small">LƯU THÔNG TIN NGƯỜI DÙNG</span>
-            </div>
-            <p class="text-uppercase small mb-5">* Hiện tại chúng tôi chỉ hỗ trợ phương thức thanh toán khi nhận hàng (COD)</p>
-        </form>
-    </div>
-    <div class="line bg-secondary"></div>
-    <div class="subTotal p-3">
-        <h2 class="mb-2">Cart</h2>
-        <div class="checkout-product mb-5">
-            <div class="checkout-product-image">
-                <img src="https://dummyimage.com/600x400/000/fff" class="rounded-4">
-                <div class="checkout-quantity rounded-circle bg-primary-subtle">
-                    <div>2</div>
-                </div>
-            </div>
-            <div class="checkout-product-details">
-                <div class="checkout-product-infor">
-                    <div class="checkout-product-name">Nhẫn Bạc S925 Winter Lotus Ring Helios Silver Original</div>
-                    <p class="text-small">Chất liệu</p>
-                    <p class="text-small">Size</p>
-                    <p class="text-small">Màu</p>
-                </div>
-            </div>
-            <div class="checkout-product-price text-end">
+        </div>
+        <div class="subTotal bg-body-tertiary rounded-3 position-sticky col-md-4 col-sm-12 p-3">
+            <div class="border-bot d-flex justify-content-between fs-3 fw-bold mb-4">
+                <div>Total Price</div>
                 <div>9.999.999$</div>
             </div>
-        </div>
-        <div class="subtotal-transport mb-5">
-            <div class="checkout-subtotal d-flex justify-content-between mb-3">
-                <div class="text-uppercase fw-bold small">Sub Total</div>
-                <div>9.999.999$</div>
-            </div>
-            <div class="checkout-transport d-flex justify-content-between mb-3">
-                <div class="text-uppercase fw-bold small">Transport</div>
-                <div>Free</div>
-            </div>
-        </div>
-        <div class="checkout-total d-flex justify-content-between mb-3">
-            <div class="text-uppercase fs-5 fw-bold">Total</div>
-            <div>9.999.999$</div>
-        </div>
-        <div class="text-center">
-            <input type="submit" value="Checkout" class="btn btn-success w-75"></input>
-        </div>
+            <div class="fs-6 mb-3">
+                <div class="d-flex justify-content-between">
+                    <div>Product Price:</div>
+                    <div class="fw-bold">99.999$</div>
     </div>
-</div>
+                <div class="d-flex justify-content-between">
+                    <div>Delivery:</div>
+                    <div class="fw-bold">99.999$</div>
+                </div>
+            </div>
+            <div class="fs-6 fw-bold">Cart</div>
+            <div class="border-bot mb-3">
+                <div class="d-flex row mb-3">
+                    <img class="col-3" src="" alt="image">
+                    <div class="col-7">
+                        <div>Product Name</div>
+                        <p class="small">Material</p>
+                        <p class="small">Color</p>
+                        <p class="small">Size</p>
+                    </div>
+                    <div class="col-2 text-end">Price</div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mb-4">
+                <div>Payment Method</div>
+                <div class="fw-bold fs-5">Cash On Delivery</div>
+            </div>
+            <button class="btn btn-success w-100 rounded-pill mb-4">Checkout</button>
+            <button class="btn btn-outline-dark w-100 rounded-pill">Back</button>
+        </div>
+    </form>
+    <script>
+        $(document).ready(function() {
+            $('input').on('blur', function() {
+                var userInput = $(this).val().trim();
+
+                if (!userInput) {
+                    $(this).addClass('is-invalid');
+                } else {
+                    $(this).removeClass('is-invalid');
+                }
+            });
+        });
+    </script>
 @endsection
