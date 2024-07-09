8c56e287327cbe4881cfcd87c8d6177459207ddc
phanthanhcongo <cong.pt.2412@aptechlearning.edu.vn>
2024-07-09 12:39:30 +0700

    Added Images About

--- app/Livewire/User/About.php
+++ app/Livewire/User/About.php
@@ -3,11 +3,53 @@
 namespace App\Livewire\User;
 
 use Livewire\Component;
+use App\Models\Image;
 
 class About extends Component
 {
+    public $section_1;
+    public $section_2;
+    public $section_3;
+    public $section_4;
+    public $section_5;
+
+    public function mount()
+    {
+        $this->section_1 = Image::where('type', 'About')
+                                      ->where('description', 'About_section_1')
+                                      ->pluck('path')
+                                      ->toArray();
+
+        // dd($this->section_1);
+        $this->section_2 = Image::where('type', 'About')
+                                      ->where('description', 'About_section_2')
+                                      ->pluck('path')
+                                      ->toArray();
+
+        $this->section_3 = Image::where('type', 'About')
+                                      ->where('description', 'About_section_3')
+                                      ->pluck('path')
+                                      ->toArray();
+
+        $this->section_4 = Image::where('type', 'About')
+                                      ->where('description', 'About_section_4')
+                                      ->pluck('path')
+                                      ->toArray();
+        $this->section_5 = Image::where('type', 'About')
+                                      ->where('description', 'About_section_5')
+                                      ->pluck('path')
+                                      ->toArray();
+
+    }
+
     public function render()
     {
-        return view('livewire.user.about');
+        return view('livewire.user.about', [
+            'section_1' => $this->section_1,
+            'section_2'=> $this->section_2,
+            'section_3'=>$this->section_3,
+            'section_4'=>$this->section_4,
+            'section_5'=>$this->section_5,
+        ]);
     }
 }
